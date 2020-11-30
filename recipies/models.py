4bf521dbeebe3c37dac8a6f75c8827a8c969011a
Kasper Nyhus <kanyhus@gmail.com>
2020-11-30 22:21:59 +0100

    models init

--- recipies/models.py
+++ recipies/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+
+class RecipeTypes(models.Model):
+  type_name = models.CharField(max_length=50)
+
+
+class Recipies(models.Model):
+  name = models.CharField(max_length=50)
+  description = models.CharField(max_length=300)
+  recipe_type = models.ForeignKey(RecipeTypes, on_delete=models.DO_NOTHING, blank=True)
+
+  def __str__(self):
+    return self.name
+
+
+class Ingredients(models.Model):
+  name = models.CharField(max_length=50)
+  description = models.CharField(max_length=300)
+  price = models.FloatField(blank=True)
+
+  def __str__(self):
+    return self.name
+
+
+class MeasurementUnits(models.Model):
+  unit_name = models.CharField(max_length=50)
+
+
+class RecipeIngredients(models.Model):
+  recipe = models.ForeignKey(Recipies, on_delete=models.CASCADE)
+  ingredient = models.ForeignKey(Ingredients, on_delete=models.DO_NOTHING)
+  measurement_unit = models.ForeignKey(MeasurementUnits, on_delete=models.DO_NOTHING, blank=False, default=1)
+  amount = models.FloatField(default=1, blank=False)
+
+
+class RecipeInstructions(models.Model):
+  recipe = models.ForeignKey(Recipies, on_delete=models.CASCADE)
+  step = models.IntegerField(default=1, blank=False)
+  description = models.CharField(max_length=2000)
+
+
