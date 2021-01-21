04c7817b61eb819ee2a37699a0c86757fb7f64df
Kasper Nyhus <kanyhus@gmail.com>
2021-01-21 23:33:20 +0100

    admin ingredientcategories

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from .models import *
 
 
-
-
-
 class IngredientsAdmin(admin.ModelAdmin):
   list_display = ('id', 'name', 'description', 'category', 'best_before', 'price')
 
-  
 
+class IngredientCategoryAdmin(admin.ModelAdmin):
+  list_display = ('name', 'shop_order')
+
+  
 admin.site.register(Ingredients, IngredientsAdmin)
+admin.site.register(IngredientCategory, IngredientCategoryAdmin)
